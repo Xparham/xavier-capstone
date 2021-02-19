a2c24fd7af4843bc58cf85ecb58daced05eed0e9
Xparham <xparham@gmail.com>
2021-02-19 17:07:51 -0500

    edited flex, edited nav text color,

--- src/components/ui/MainNav.js
+++ src/components/ui/MainNav.js
@@ -1,9 +1,9 @@
 export const MainNav =() => (
-    <div className="bg-gray-600 py-6">
+    <div className="bg-gray-600 py-6 flex justify-between">
         <div>
-            <h1 className="px-10 flex text-4xl text-white">Stork</h1>
+            <h1 className="px-10 text-4xl text-white">Stork</h1>
         </div>
-        <ul>
+        <ul className="flex space-x-8 text-white">
             <li>Home</li>
             <li>Search</li>
             <li></li>

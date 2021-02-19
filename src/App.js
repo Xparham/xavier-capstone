411e2850520ce91f5a16f1a9bdde3f8ebd8ab5e6
Xparham <xparham@gmail.com>
2021-02-19 16:56:49 -0500

    Added fragment

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="p-24">
-      <h1 className="px-10 flex text-4xl">Welcome Back</h1>
-    </div>
+    <>
+      <div className="p-24">
+        <h1 className="px-10 flex text-4xl">Welcome Back</h1>
+      </div>
+    </>
   );
 }
 

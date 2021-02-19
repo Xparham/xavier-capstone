801c3b9e86b459b386179e7666b459dafc5394f1
Xparham <xparham@gmail.com>
2021-02-19 16:48:30 -0500

    edited background color

---
+++ src/components/Footer.js
@@ -0,0 +1,67 @@
+function Footer(){
+    return(
+        <>
+            <footer class="bg-gray-600 inset-x-0 bottom-0">
+                <div class="px-12 pt-4 pb-6 mx-auto md:container">
+                    <div class="flex flex-wrap">
+                        <div class="w-full text-center md:w-1/4 md:text-left">
+                            <h5 class="mb-6 font-bold text-white uppercase">Links</h5>
+                            <ul class="mb-4">
+                                <li class="mt-2">
+                                <a href="/" class="text-white hover:underline hover:text-yellow-400">FAQ</a>
+                                </li>
+                                <li class="mt-2">
+                                <a href="/" class="text-white hover:underline hover:text-yellow-400">Help</a>
+                                </li>
+                                <li class="mt-2">
+                                <a href="/S" class="text-white hover:underline hover:text-yellow-400">Support</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="w-full text-center md:w-1/4 md:text-left">
+                            <h5 class="mb-6 font-bold text-white uppercase">Legal</h5>
+                            <ul class="mb-4">
+                                <li class="mt-2">
+                                <a href="/" class="text-white hover:underline hover:text-yellow-400">Terms</a>
+                                </li>
+                                 <li class="mt-2">
+                                 <a href="/" class="text-white hover:underline hover:text-yellow-400">Privacy</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="w-full text-center md:w-1/4 md:text-left">
+                            <h5 class="mb-6 font-bold text-white uppercase">Social</h5>
+                            <ul class="mb-4">
+                                 <li class="mt-2">
+                                <a href="/" class="text-white hover:underline hover:text-yellow-400">Facebook</a>
+                                </li>
+                                 <li class="mt-2">
+                                 <a href="/" class="text-white hover:underline hover:text-yellow-400">Linkedin</a>
+                                </li>
+                                <li class="mt-2">
+                                 <a href="/" class="text-white hover:underline hover:text-yellow-400">Twitter</a>
+                                </li>
+                             </ul>
+                        </div>
+                        <div class="w-full text-center md:w-1/4 md:text-left">
+                            <h5 class="mb-6 font-bold text-white uppercase">Company</h5>
+                            <ul class="mb-4">
+                                <li class="mt-2">
+                                <a href="/" class="text-white hover:underline hover:text-yellow-400">Official Blog</a>
+                                </li>
+                                <li class="mt-2">
+                                <a href="/" class="text-white hover:underline hover:text-yellow-400">About Us</a>
+                                </li>
+                                <li class="mt-2">
+                                <a href="contact.html" class="text-white hover:underline hover:text-yellow-400">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer

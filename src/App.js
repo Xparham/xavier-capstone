ed42ca046b76ba04b6b426f0970face07009fae1
Xparham <xparham@gmail.com>
2021-02-19 16:59:05 -0500

    imported footer and header js files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { MainNav } from './components/ui/MainNav';
+import Footer from './components/ui/Footer';
 
 function App() {
   return (
     <>
+      <MainNav/>
       <div className="p-24">
         <h1 className="px-10 flex text-4xl">Welcome Back</h1>
       </div>
+      <Footer/>
     </>
   );
 }

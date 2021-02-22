9d72d0c25dacdb27f4d9225c1915e2e6d1ea79e7
Xparham <xparham@gmail.com>
2021-02-22 18:07:17 -0500

    Added fragment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 export const Home = () => (
-    <div className="flex justify-center items-center flex-col space-y-6 p-24">
-        <h1 className="px-10 text-4xl">Welcome Back</h1>
-        <p>Begin searching for the nearest food banks and pantries starting here.</p>
-        <button className="border-2 p-2 bg-green-500">Search Now</button>
-
-    </div>
+    <>
+        <div className="flex justify-center items-center flex-col space-y-6 p-24">
+            <h1 className="px-10 text-4xl">Welcome Back</h1>
+            <p>Begin searching for the nearest food banks and pantries starting here.</p>
+            <button className="border-2 p-2 bg-green-500">Search Now</button>
+        </div>
+    </>
 )

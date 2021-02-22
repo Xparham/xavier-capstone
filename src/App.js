96f45c6e9ecc5c80ef52aebda72fad84fda246a8
Xparham <xparham@gmail.com>
2021-02-22 16:33:26 -0500

    added button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { MainNav } from './components/ui/MainNav';
-import Footer from './components/ui/Footer';
+import { MainNav, Footer } from './components/ui/';
+
 
 function App() {
   return (
     <>
       <MainNav/>
-      <div className="p-24">
-        <h1 className="px-10 flex text-4xl">Welcome Back</h1>
+      <div className="flex justify-center items-center flex-col space-y-6 p-24">
+        <h1 className="px-10 text-4xl">Welcome Back</h1>
+        <p>Begin searching for the nearest food banks and pantries starting here.</p>
+        <button className="border-2 p-2 bg-green-500">Search Now</button>
+
       </div>
       <Footer/>
     </>

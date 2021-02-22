4ed7014f7a0aceefae757eb72f4bb99365a1b7ae
Xparham <xparham@gmail.com>
2021-02-22 17:32:58 -0500

    fixed broken closing tag

--- src/pages/search/FoodSearch.js
+++ src/pages/search/FoodSearch.js
@@ -1,7 +1,8 @@
 export const FoodSearch =() => (
+    
    <form>
        <p><strong>Look For Nearest Pantries, Banks, Organizations etc.</strong></p>
-       <input className="w-72 py-2" type="text" placeholder="City, State, Zip-Code, etc.">
+       <input className="w-72 py-2" type="text" placeholder="City, State, Zip-Code, etc."></input>
         <button className="border-2 border-white text-white p-2 cursor-pointer bg-green-600 hover:bg-blue-600">Search</button>
    </form>
 )

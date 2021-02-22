d449c02e3cbeec914d9f63bd49568a02143f48b9
Xparham <xparham@gmail.com>
2021-02-22 18:50:51 -0500

    imported router and established links

--- src/components/ui/MainNav.js
+++ src/components/ui/MainNav.js
@@ -1,12 +1,13 @@
+import{Link} from 'react-router-dom'
 export const MainNav =() => (
     <div className="bg-gray-600 py-6 flex justify-between">
         <div>
             <h1 className="px-10 text-4xl text-white">Stork</h1>
         </div>
         <ul className="flex space-x-8 text-white px-10">
-            <li>Home</li>
+            <li><Link to="/">Home</Link></li>
             <li>(AVATAR)</li>
-            <li><strong>Contact Us</strong></li>
+            <li><Link to="/contact-us"><strong>Contact Us</strong></Link></li>
         </ul>
     </div>
 )

196677e81dec642cd357223eb72656317fbc934d
Xparham <xparham@gmail.com>
2021-02-22 18:37:51 -0500

    imported header and footer

--- src/pages/search/FoodSearch.js
+++ src/pages/search/FoodSearch.js
@@ -1,8 +1,13 @@
+import { MainNav, Footer } from "../../components/ui";
+
 export const FoodSearch =() => (
-    
-   <form>
-       <p><strong>Look For Nearest Pantries, Banks, Organizations etc.</strong></p>
-       <input className="w-72 py-2" type="text" placeholder="City, State, Zip-Code, etc."></input>
-        <button className="border-2 border-white text-white p-2 cursor-pointer bg-green-600 hover:bg-blue-600">Search</button>
-   </form>
+   <>
+      <MainNav/>
+      <form className="flex justify-start items-center px-12 p-6 border-b-2 border-black bg-green-800 space-x-8">
+         <p className="text-white"><strong>Look For Nearest Pantries, Banks, Organizations etc.</strong></p>
+         <input className="w-72 py-2" type="text" placeholder="City, State, Zip-Code, etc."></input>
+         <button className="border-2 border-white text-white p-2 cursor-pointer bg-green-600 hover:bg-blue-600">Search</button>
+      </form>
+      <Footer/>
+   </>
 )

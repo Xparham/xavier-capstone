2e4c5e3544d9e33f656fbadb856cb235a26466cc
Xparham <xparham@gmail.com>
2021-02-22 18:46:50 -0500

    added link to button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import { MainNav, Footer } from "../components/ui";
+import {Link} from 'react-router-dom'
 
 export const Home = () => (
     <>
@@ -6,7 +7,7 @@ export const Home = () => (
         <div className="flex justify-center items-center flex-col space-y-6 p-24">
             <h1 className="px-10 text-4xl">Welcome Back</h1>
             <p>Begin searching for the nearest food banks and pantries starting here.</p>
-            <button className="border-2 p-2 bg-green-500">Search Now</button>
+            <button className="border-2 p-2 bg-green-500"><Link to="/search">Search Now</Link></button>
         </div>
         <Footer/>
     </>

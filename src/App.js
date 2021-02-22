6b876e0e5fc151b8c2578e79f70bf0bea9570112
Xparham <xparham@gmail.com>
2021-02-22 18:14:37 -0500

    imported router

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import { MainNav, Footer } from './components/ui/';
 
 function App() {
   return (
-    <>
-      <MainNav/>
-      <div className="flex justify-center items-center flex-col space-y-6 p-24">
-        <h1 className="px-10 text-4xl">Welcome Back</h1>
-        <p>Begin searching for the nearest food banks and pantries starting here.</p>
-        <button className="border-2 p-2 bg-green-500">Search Now</button>
-
-      </div>
-      <Footer/>
-    </>
+  
   );
 }
 

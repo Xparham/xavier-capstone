6018cb8da87776bdb221d931ef19ddeddaf6e14f
Xparham <xparham@gmail.com>
2021-02-22 17:28:48 -0500

    added button and inpt fieldh

--- src/pages/search/FoodSearch.js
+++ src/pages/search/FoodSearch.js
@@ -1,5 +1,7 @@
 export const FoodSearch =() => (
    <form>
        <p><strong>Look For Nearest Pantries, Banks, Organizations etc.</strong></p>
+       <input className="w-72 py-2" type="text" placeholder="City, State, Zip-Code, etc.">
+        <button className="border-2 border-white text-white p-2 cursor-pointer bg-green-600 hover:bg-blue-600">Search</button>
    </form>
 )

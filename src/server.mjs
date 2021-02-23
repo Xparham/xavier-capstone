03971cde5bb592d5b9cc522bff588bacc8ba661d
Xparham <xparham@gmail.com>
2021-02-23 16:15:51 -0500

    Defined Port and Host

--- src/server.mjs
+++ src/server.mjs
@@ -3,3 +3,6 @@ import express from 'express'
 const app = express()
 const PORT = process.env.PORT || 8080
 const HOST = process.env.HOST || '0.0.0.0'
+
+app.get("/", (req, res) => res.send({response : "OK"}))
+app.listen(PORT,() => console.log(`Server run on http://${HOST}:${PORT}`))

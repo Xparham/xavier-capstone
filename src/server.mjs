429b6886e96cf9ca35a25196581b50e6bc7ce3fd
Xparham <xparham@gmail.com>
2021-02-23 17:58:28 -0500

    checkpoint

--- src/server.mjs
+++ src/server.mjs
@@ -5,4 +5,7 @@ const PORT = process.env.PORT || 8080
 const HOST = process.env.HOST || '0.0.0.0'
 
 app.get("/", (req, res) => res.send({response : "OK"}))
+
+app.get("/search", (req, res) => res.send({search:"Food Banks Available"}))
+
 app.listen(PORT,() => console.log(`Server run on http://${HOST}:${PORT}`))
